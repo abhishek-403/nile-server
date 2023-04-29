5c299fc0e7ccc11bdb35d53952c8e8620d925e0e
Abhishek Sharma <abhishek79009@gmail.com>
2023-04-29 13:21:54 +0530

    logout added

--- controller/authController.js
+++ controller/authController.js
@@ -3,118 +3,148 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 const { success, error } = require('../utils/responseWrapper');
 
-const signUpController = async (req,res)=>{
+const signUpController = async (req, res) => {
 
     try {
-        const {name,email,password} = req.body;
-        if(!email || !password){
-            return res.send(error(400,"All fields required!"))
+        const { name, email, password } = req.body;
+        if (!email || !password) {
+            return res.send(error(400, "All fields required!"))
         }
 
 
-        const invalid = await User.findOne({email});
+        const invalid = await User.findOne({ email });
 
-        if(invalid){
-            return res.send(error(409,"User already exists."))
+        if (invalid) {
+            return res.send(error(409, "User already exists."))
 
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-              
-        
-        const currUser= await User.create({
-            name,email,password:hashedPassword
+
+
+        const currUser = await User.create({
+            name, email, password: hashedPassword
         })
 
-        const accessToken = createAccessToken({_id:currUser._id})
-        const refreshToken = createRefreshToken({_id:currUser._id})
+        const accessToken = createAccessToken({ _id: currUser._id })
+        const refreshToken = createRefreshToken({ _id: currUser._id })
 
         res.cookie('jwt', refreshToken, {
             httpOnly: true,
             secure: true
         })
-        
-        return res.send(success(201,{accessToken}))
-    } 
+
+        return res.send(success(201, { accessToken }))
+    }
     catch (e) {
         return res.send(error(500, e.message))
-        
+
     }
 
 
 
 }
 
-const loginController = async (req,res)=>{
+const loginController = async (req, res) => {
 
     try {
 
 
-        const {email,password} = req.body;
-        if(!email || !password){
-            return res.send(error(400,"All field required!"))
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.send(error(400, "All field required!"))
         }
 
-        const currUser = await User.findOne({email}).select('+password');
+        const currUser = await User.findOne({ email }).select('+password');
 
-        if(!currUser){
-            return res.send(error(404,"User not found!"))
+        if (!currUser) {
+            return res.send(error(404, "User not found!"))
         }
 
-        const compare = await bcrypt.compare(password,currUser.password);
+        const compare = await bcrypt.compare(password, currUser.password);
 
-        if(!compare){
-            return res.send(error(401,"Wrong Password!"))
+        if (!compare) {
+            return res.send(error(401, "Wrong Password!"))
         }
 
-        
-       
-
-
-        return res.send(success(200,{currUser}))
 
 
 
 
+        return res.send(success(200, { currUser }))
 
 
-        
+
+
+
+
+
     } catch (e) {
         return res.send(error(500, e.message))
     }
-    
+
 
 
 }
 
-const RefreshAccessToken=async (req,res)=>{
+
+const logOutController = async (req, res) => {
+
     try {
-        const cookies= req.cookies;
-        const refreshToken= cookies.jwt;
 
-        if(!refreshToken){
-            return res.send(error(401,"Refresh Token required"))
-        }
-        
+        res.clearCookie('jwt', {
+            httpOnly: true,
+            secure: true
+        })
+        return res.send(success(200, 'User logged out'))
+
     } catch (e) {
         return res.send(error(500, e.message))
-        
+
+    }
+}
+
+
+
+
+
+
+
+
+
+const refreshAccessTokenController = async (req, res) => {
+    try {
+        const cookies = req.cookies;
+        const refreshToken = cookies.jwt;
+
+        if (!refreshToken) {
+            return res.send(error(401, "Refresh Token required"))
+        }
+
+    } catch (e) {
+        return res.send(error(500, e.message))
+
     }
 
 }
 
-function createAccessToken (body){
-    const accessToken = jwt.sign(body,process.env.ACCESS_TOKEN_KEY,{
-        expiresIn:'1h'
+
+
+
+
+
+function createAccessToken(body) {
+    const accessToken = jwt.sign(body, process.env.ACCESS_TOKEN_KEY, {
+        expiresIn: '1h'
     })
     return accessToken;
-    
+
 }
 
-function createRefreshToken(body){
-    const refreshToken = jwt.sign(body,process.env.REFRESH_TOKEN_KEY,{
-        expiresIn:'1y'
+function createRefreshToken(body) {
+    const refreshToken = jwt.sign(body, process.env.REFRESH_TOKEN_KEY, {
+        expiresIn: '1y'
     })
 
     return refreshToken;
@@ -125,8 +155,10 @@ function createRefreshToken(body){
 
 
 
-module.exports ={
+module.exports = {
     signUpController,
-    loginController
+    loginController,
+    refreshAccessTokenController,
+    logOutController
 
 }
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const authController = require('../controller/authController')
 
-router.post('/signup',authController.signUpController)
-router.post('/login',authController.loginController)
+router.post('/signup', authController.signUpController)
+router.post('/login', authController.loginController)
+router.post('/logout', authController.logOutController)
+router.post('/refres', authController.refreshAccessTokenController)
 
 module.exports = router;
